6a1e5ffa2bd315d51c57aec93e26447f70619c27
Vitaly <ivanov.arduan@yandex.ru>
2022-10-19 17:42:21 +0300

    Second commit.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreatForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreatForm
+    form = CustomUserChangeForm
+    list_display = ['email', 'username', 'age']
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
