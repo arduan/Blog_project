0f93b5a56e8fdb7a5ea35b7a4ec3ac2a9f927717
Vitaly <ivanov.arduan@yandex.ru>
2022-10-21 09:41:00 +0300

    Good.

--- news/settings.py
+++ news/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
     'users.apps.UsersConfig',
     'pages',
 ]
@@ -122,3 +124,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
