0a97dcf7051647a410483a9414f624b29fe76a68
Vitaly <ivanov.arduan@yandex.ru>
2022-10-23 15:26:02 +0300

    Edit.

--- articles/models.py
+++ articles/models.py
@@ -7,10 +7,13 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
 
-    def get_absolut_url(self):
-        return reverse('article_detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.pk})
