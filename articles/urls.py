4f47c95804130c4e6f58cfe49e0c1e52a1aea07a
Vitaly <ivanov.arduan@yandex.ru>
2022-10-24 15:42:17 +0300

    Add html files.

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from . import views
-
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+    ArticleCreateView,  # new
+)
 
 urlpatterns = [
-    path('', views.ArticleListView.as_view(), name='article_list'),
-    path('<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article_edit'),
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("new/", ArticleCreateView.as_view(), name="article_new"),  # new
+    path("", ArticleListView.as_view(), name="article_list"),
 ]
